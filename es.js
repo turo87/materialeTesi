a795ac81d5573f0e5e4e785d877e12dad157f315
Salvatore Tambaro <salva87@alice.it>
2018-12-23 23:54:25 +0100

    23:54

--- es.js
+++ es.js
@@ -3,15 +3,17 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.amazon.it/s/ref=nb_sb_noss?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=search-alias%3Daps&field-keywords=iphone');
+    await page.goto('file:///Users/salvatore/Desktop/Amazon.it%20%20iphone.html');
 
     // await page.$x() returns array of ElementHandle
-    const featureArticles = (await page.$x('//h2'));
+    const featureArticles = (await page.$x("//li[@class='s-result-item celwidget  ']"));
+    
     
     for(var i=0; i<featureArticles.length; i++) {
 
         const text = await page.evaluate(el => {
             // do what you want with featureArticles in page.evaluate
+        	
             return el.textContent;
         }, featureArticles[i]);
 
