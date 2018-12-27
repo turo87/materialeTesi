6cff877ea527557b88f002d40339231b0f823c6b
Salvatore Tambaro <salva87@alice.it>
2018-12-27 23:46:02 +0100

    27/12 23:45

--- es.js
+++ es.js
@@ -1,24 +1,35 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('file:///Users/salvatore/Desktop/Amazon.it%20%20iphone.html');
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('file:///Users/salvatore/Desktop/Amazon.it%20%20iphone.html');
 
-    // await page.$x() returns array of ElementHandle
-    const featureArticles = (await page.$x("//li[@class='s-result-item celwidget  ']"));
-    
-    
-    for(var i=0; i<featureArticles.length; i++) {
 
-        const text = await page.evaluate(el => {
-            // do what you want with featureArticles in page.evaluate
-        	
-            return el.textContent;
-        }, featureArticles[i]);
+	// await page.$x() returns array of ElementHandle
+	const articles = (await page.$x("//li[@class='s-result-item celwidget  ']"));
 
-        console.log(text);
-    }
+	for(var i=0; i<articles.length; i++) {
 
-    await browser.close();
+		const featureArticleTitle = (await page.$x("//li[@class='s-result-item celwidget  ']//h2"))[i];
+		const title = await page.evaluate(el => {
+			// do what you want with featureArticleTitle in page.evaluate
+			return el.textContent;
+		}, featureArticleTitle);
+
+		console.log(title);
+
+
+		const featureArticlePrice = (await page.$x("//span[@class='a-size-base a-color-price s-price a-text-bold']"))[i];
+		const price = await page.evaluate(el => {
+			// do what you want with featureArticlePrice in page.evaluate
+			return el.textContent;
+		}, featureArticlePrice);
+
+		console.log(price);
+
+		
+	}
+
+	await browser.close();
 })();
