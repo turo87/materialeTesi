df5325bbe62f423fc32c0f91db6ebcbdca2872fb
Salvatore Tambaro <salva87@alice.it>
2019-01-25 11:14:54 +0100

    25/01

--- booking1_5.js
+++ booking1_5.js
@@ -2,21 +2,21 @@
  * 
  */
 
-
 const puppeteer = require('puppeteer');
 
 let bookingUrl = 'https://www.booking.com/index.it.html?label=gen173nr-1BCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQ_YAQHoAQGIAgGoAgM;sid=9e5a2f72fb374b24b8be4033628b9dd1;keep_landing=1&sb_price_type=total&';
 (async () => {
 
-	//let me to listen from console, also within in 'evaluate()' function
+	// Let me to listen from console, also within in 'evaluate()' function
 //	page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
 	const browser = await puppeteer.launch({ headless: true }); // headless mode disabled
+
 	//wait for opening new page
 	const page = await browser.newPage();
+
 	//wait for goto url
 	await page.goto(bookingUrl);
-
 
 	// Type into search box.
 	await page.type("#ss", 'Cosenza'); // Types slower, like a user
@@ -30,22 +30,16 @@ let bookingUrl = 'https://www.booking.com/index.it.html?label=gen173nr-1BCAEoggI
 	const resultsSelector = '#hotellist_inner';
 	await page.waitForSelector(resultsSelector);
 
-
-
-	/*    start to select elements from the page    */
-
-
-	//selection <Promise<Array<ElementHandle>>>
+	//selection <Promise<Array<ElementHandle>>> within page
 	const hotelsIds = (await page.$x("//div[@data-hotelid]"));
 //	console.log("lunghezza array di tutti gli hotel trovati: " + hotelsIds.length);
 
 	var hotels = [];
 
+	/*  start to select elements within elementHandle  */
 
 	for(var i=0; i<hotelsIds.length; i++) {
-
 		var hotelJson = {};
-
 		var nameFeature = await hotelsIds[i].$x("./descendant-or-self::span[@class='sr-hotel__name\n'][1]");
 		var name = await page.evaluate(el => {
 			return el.textContent;
@@ -67,6 +61,9 @@ let bookingUrl = 'https://www.booking.com/index.it.html?label=gen173nr-1BCAEoggI
 		hotels.push(hotelJson);
 	}
 
+	/*  end to select elements within elementHandle  */
+
+	// extraction elements
 	hotels.forEach((hotelElement) => {
 		console.log(hotelElement.name + " " + hotelElement.bui);
 	})
