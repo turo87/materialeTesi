356387e727d0235f77d10e42f809ac32f4319b69
Salvatore Tambaro <salva87@alice.it>
2019-03-04 15:52:27 +0100

    04/03 15:52

--- testCase.js
+++ testCase.js
@@ -18,7 +18,7 @@ const input2 = [
 	{"scrape":
 	{"allrooms":["./descendant-or-self::*[@class='jqrt togglelink']"]}}];
 var output2 = {"allrooms":
-"\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\nCamera Familiare\n2 letti singoli  e  1 letto matrimoniale\nJunior Suite\n1 divano letto  e 1 letto matrimoniale\nSuite\n2 letti singoli  e 1 letto matrimoniale\nCamera Matrimoniale Business"};
+"\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\nCamera Familiare\n\nJunior Suite\n\nSuite\n\nCamera Matrimoniale Business"};
 
 const input3 = [
 	{"open": "https://www.booking.com/hotel/it/holiday-inn-cosenza.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQ_YAQHoAQH4AQuIAgGoAgM;sid=9e5a2f72fb374b24b8be4033628b9dd1;dest_id=-116404;dest_type=city;dist=0;hapos=1;hpos=1;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1550836160;srpvid=c4ce532049eb0091;type=total;ucfs=1&#hotelTmpl"},
@@ -27,24 +27,29 @@ const input3 = [
 		"name" : "//*[@class='hp__hotel-name']",
 		"address" : "//*[@class='\nhp_address_subtitle\njs-hp_address_subtitle\njq_tooltip\n']"}}];
 var output3 = {"allrooms":
-	"\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\nCamera Familiare\n2 letti singoli  e  1 letto matrimoniale\nJunior Suite\n1 divano letto  e 1 letto matrimoniale\nSuite\n2 letti singoli  e 1 letto matrimoniale\nCamera Matrimoniale Business",
+	"\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\nCamera Familiare\n\nJunior Suite\n\nSuite\n\nCamera Matrimoniale Business",
 	"name":"\nHotel\nItaliana Hotels Cosenza\n",
 	"address":"\nVia Panebianco 452, 87100 Cosenza, Italia\n"};
 
 const input4 = [
 	{"open": "https://www.booking.com/hotel/it/holiday-inn-cosenza.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQ_YAQHoAQH4AQuIAgGoAgM;sid=9e5a2f72fb374b24b8be4033628b9dd1;dest_id=-116404;dest_type=city;dist=0;hapos=1;hpos=1;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1550836160;srpvid=c4ce532049eb0091;type=total;ucfs=1&#hotelTmpl"},
 	{"scrape":
-	{"allrooms":["//*[@class='jqrt togglelink']"]}}];
-var output4 = {"allrooms":
-"\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\nCamera Familiare\n2 letti singoli  e  1 letto matrimoniale\nJunior Suite\n1 divano letto  e 1 letto matrimoniale\nSuite\n2 letti singoli  e 1 letto matrimoniale\nCamera Matrimoniale Business"};
+	{"rooms": { "_forEach_" : ["./descendant-or-self::*[@class='jqrt togglelink']"],
+		"_extract_": { "type" : "./descendant-or-self::*[@class='jqrt togglelink']", 
+			"describe" : "./descendant-or-self::*[@class='bed-types-wrapper\n']"
+		}}}}];
+var output4 = {"rooms":
+"\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\n2 letti singoli  oppure 1 letto matrimoniale Camera Familiare\n2 letti singoli  e  1 letto matrimoniale\nJunior Suite\n1 divano letto  e 1 letto matrimoniale\nSuite\n2 letti singoli  e 1 letto matrimoniale\nCamera Matrimoniale Business\n1 letto matrimoniale"};
 
 function main() {
-	pushTestCase(input1);
-	pushResult(output1);
-	pushTestCase(input2);
-	pushResult(output2);
-	pushTestCase(input3);
-	pushResult(output3);
+//	pushTestCase(input1);
+//	pushResult(output1);
+//	pushTestCase(input2);
+//	pushResult(output2);
+//	pushTestCase(input3);
+//	pushResult(output3);
+	pushTestCase(input4);
+	pushResult(output4);
 
 	var test =  [];
 	var result = [];
