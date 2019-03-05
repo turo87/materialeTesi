0d4ce0031bd6a0b34d7e28f43ca6ebc53e44bd4e
Salvatore Tambaro <salva87@alice.it>
2019-03-05 01:40:39 +0100

    05/03 01:40

---
+++ ricorsione16.js
@@ -0,0 +1,200 @@
+/**
+ * 
+ */
+var browserOpener = require('./browserOpener.js');
+var pageOpener = require('./pageOpener.js');
+var browserCloser = require('./browserCloser');
+var testCase = require('./testCase.js');
+var arrayConstructor = [].constructor;
+var objectConstructor = {}.constructor;
+
+var browser;
+
+
+//Input: Array INP
+const test = testCase.main();
+
+//Output: Object OUT
+let output;
+
+var contexts = [];
+var currentContext = {};
+
+async function main(input) {
+	output = {};
+//	context contiene il contesto di valutazione corrente, tipo la pagina
+	for(var i=0; i<input.length; i++) {
+		if(isActionNode(input[i]))
+			await evalActionNode(input[i],currentContext);			
+		if(isScraplNode(input[i]))
+			output = await evalNode(input[i].scrape,currentContext);
+	}
+	return output;
+}
+async function evalActionNode(node,currentContext) {
+	if(Object.keys(node)=="open") {		
+		let url = node.open;
+		currentContext.page = await pageOpener.open(browser,url);
+		currentContext.currentNode = currentContext.page;
+		contexts.push(currentContext);
+	}
+}
+async function evalNode(node,currentContext) {
+	if (node === null) {
+		return null;
+	}
+	if(isForeach(node)) {
+		return await evalForEach(node,currentContext);
+	}
+	if(isObject(node)) {
+		return await evalObject(node,currentContext);		
+	}
+	if(isArray(node)) {
+		return await evalArray(node,currentContext);
+	}
+	return await evalAtomicValue(node,currentContext);
+}
+
+async function evalForEach(node,currentContext) {
+	var a = [];
+//	console.log("evalForeach:");
+//	console.log(node);
+	var values = Object.values(node);
+	var _for = values[0];
+//	console.log("_for:");
+//	console.log(_for);
+	var _extract = values[1];
+//	console.log("_EXTRACT:");
+//	console.log(_extract);
+	var array = await evalNode(_for,currentContext);
+	var arrayElements = await currentContext.currentNode.$x(_for);
+//	console.log("ARRAYYYYY");
+//	console.log(array);
+//	console.log(arrayElements);
+	for(var i=0; i<array.length; i++) {
+//		a.push(array[i]);
+		var context1 = {};
+//		console.log(arrayElements[i])
+		context1.currentNode = arrayElements[i];
+		context1.page = currentContext.page;
+		contexts.push(context1);
+		currentContext = context1;
+//		console.log("FOR + EXTRACT");
+//		console.log(_for+_extract);
+		a.push(await evalNode(_extract,currentContext));		
+		contexts.pop();
+		currentContext = contexts[contexts.length-1];
+	}
+//	console.log("OUTTTTTT");
+//	console.log(a);
+	return await a;
+//	await console.log(array);
+}
+
+async function evalArray(node,currentContext) {
+	var out = [];
+	for(var j=0; j<node.length; j++) {
+		var array = await currentContext.currentNode.$x(node[j]);
+		for(var i=0; i<array.length; i++) {
+//			var context1 = {};
+//			context1.currentNode = array[i];
+//			context1.page = currentContext.page;
+//			contexts.push(context1);
+//			currentContext = context1;
+			var string = "(" + node[j] + ")" + "[" + (i+1) + "]";
+//			console.log("STRING:");
+//			console.log(string);
+			var d = await evalNode(string, currentContext);
+//			contexts.pop();
+//			currentContext = contexts[contexts.length-1];
+			await out.push(d);
+		}
+	}
+	return await out;
+}
+async function evalAtomicValue(selector,currentContext) {
+//	console.log("isAtomicValue:");
+//	console.log(selector);
+	try {
+		var c = await currentContext.currentNode.$x(selector);
+		var d = await currentContext.page.evaluate(el => {
+			return el.textContent;
+		},c[0]);
+	} catch(exception) {
+		console.log("NULL");
+	}
+	return d;
+}
+async function evalObject(node,currentContext) {
+//	console.log("evalObject:");
+//	console.log(node);
+	var out = {};
+//	ricorsione su ogni key
+	var keys = Object.keys(node);
+	for(var j in keys) {
+		var key = keys[j];
+//		var value = node[key].toString();
+		out[key] = await evalNode(node[key],currentContext);
+	}
+	return out;
+}
+function isObject(node) {
+//	console.log("isObject:");
+//	console.log(node);
+	if (node.constructor === objectConstructor) {
+//		console.log("YES");
+		return true;
+	}
+//	console.log("NO");
+	return false;
+}
+function isArray(node) {
+//	console.log("isArray:");
+//	console.log(node);
+	if (node.constructor === arrayConstructor) {
+//		console.log("Yes");
+		return true;
+	}
+//	console.log("NO");
+	return false;
+}
+function isScraplNode(obj) {
+	if(Object.keys(obj)=="scrape")
+		return true;
+	return false;
+}
+function isActionNode(obj) {
+	if(!isScraplNode(obj))
+		return true;
+	return false;
+}
+function isForeach(obj) {
+//	console.log("isForeach:");
+//	console.log(obj);
+//	console.log(Object.keys(obj)[0]);
+	if(Object.keys(obj)[0]=="_forEach_" && Object.keys(obj)[1]=="_extract_") {
+//		console.log("YES");
+		return true;
+	}
+//	console.log("NO");	
+	return false;
+}
+
+async function start() {
+//	console.log("numero testcase:");
+//	console.log(test.length);
+	browser = await browserOpener.open();
+	for(var i=0; i<test.length; i++) {
+		console.log("Testcase: " + i + "\nJSON aspettato:");
+		console.log(testCase.getResults()[i]);
+		var m = await main(test[i]);
+		await console.log("JSON restituito: ");
+//		await console.log(JSON.stringify(m));
+		console.log(m);
+	}
+	await browserCloser.close(browser);
+}
+
+(async () => {
+	start();
+})();
--- testCase.js
+++ testCase.js
@@ -17,39 +17,102 @@ const input2 = [
 	{"open": "https://www.booking.com/hotel/it/holiday-inn-cosenza.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQ_YAQHoAQH4AQuIAgGoAgM;sid=9e5a2f72fb374b24b8be4033628b9dd1;dest_id=-116404;dest_type=city;dist=0;hapos=1;hpos=1;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1550836160;srpvid=c4ce532049eb0091;type=total;ucfs=1&#hotelTmpl"},
 	{"scrape":
 	{"allrooms":["./descendant-or-self::*[@class='jqrt togglelink']"]}}];
-var output2 = {"allrooms":
-"\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\nCamera Familiare\n\nJunior Suite\n\nSuite\n\nCamera Matrimoniale Business"};
-
+var output2 = { "allrooms":
+	[ "'\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\n'",
+		"'\n\nCamera Familiare\n'",
+		"'\n\nJunior Suite\n'",
+		"'\n\nSuite\n'",
+		"'\n\nCamera Matrimoniale Business\n'" ]};
 const input3 = [
 	{"open": "https://www.booking.com/hotel/it/holiday-inn-cosenza.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQ_YAQHoAQH4AQuIAgGoAgM;sid=9e5a2f72fb374b24b8be4033628b9dd1;dest_id=-116404;dest_type=city;dist=0;hapos=1;hpos=1;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1550836160;srpvid=c4ce532049eb0091;type=total;ucfs=1&#hotelTmpl"},
 	{"scrape":
-	{"allrooms":["./descendant-or-self::*[@class='jqrt togglelink']"],
+	{"allrooms":["//*[@class='jqrt togglelink']"],
 		"name" : "//*[@class='hp__hotel-name']",
 		"address" : "//*[@class='\nhp_address_subtitle\njs-hp_address_subtitle\njq_tooltip\n']"}}];
-var output3 = {"allrooms":
-	"\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\nCamera Familiare\n\nJunior Suite\n\nSuite\n\nCamera Matrimoniale Business",
-	"name":"\nHotel\nItaliana Hotels Cosenza\n",
-	"address":"\nVia Panebianco 452, 87100 Cosenza, Italia\n"};
+var output3 = { "allrooms":
+	[ "'\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\n'",
+		"'\n\nCamera Familiare\n'",
+		"'\n\nJunior Suite\n'",
+		"'\n\nSuite\n'",
+		"'\n\nCamera Matrimoniale Business\n'" ],
+		"name":"\nHotel\nItaliana Hotels Cosenza\n",
+		"address":"\nVia Panebianco 452, 87100 Cosenza, Italia\n"};
 
 const input4 = [
 	{"open": "https://www.booking.com/hotel/it/holiday-inn-cosenza.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQ_YAQHoAQH4AQuIAgGoAgM;sid=9e5a2f72fb374b24b8be4033628b9dd1;dest_id=-116404;dest_type=city;dist=0;hapos=1;hpos=1;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1550836160;srpvid=c4ce532049eb0091;type=total;ucfs=1&#hotelTmpl"},
 	{"scrape":
-	{"rooms": { "_forEach_" : ["./descendant-or-self::*[@class='jqrt togglelink']"],
-		"_extract_": { "type" : "./descendant-or-self::*[@class='jqrt togglelink']", 
-			"describe" : "./descendant-or-self::*[@class='bed-types-wrapper\n']"
+	{"rooms": {
+		"_forEach_" : ["//table[@id='maxotel_rooms']/tbody/tr/td[2]/div/div"],
+		"_extract_": {"type" : "./a", "describe" : "./div"
 		}}}}];
-var output4 = {"rooms":
-"\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\n2 letti singoli  oppure 1 letto matrimoniale Camera Familiare\n2 letti singoli  e  1 letto matrimoniale\nJunior Suite\n1 divano letto  e 1 letto matrimoniale\nSuite\n2 letti singoli  e 1 letto matrimoniale\nCamera Matrimoniale Business\n1 letto matrimoniale"};
+var output4 = {"rooms": [
+
+	{"type" : "\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\n",
+		"describe" : "2 letti singoli  oppure 1 letto matrimoniale "},
+		{"type" : "Camera Familiare\n",
+			"describe" : "2 letti singoli  e  1 letto matrimoniale"
+		},
+		{"type" : "\nJunior Suite\n",
+			"describe" : "1 divano letto  e 1 letto matrimoniale"
+		},
+		{"type" : "\nSuite\n",
+			"describe" : "2 letti singoli  e 1 letto matrimoniale"
+		},
+		{"type" : "\nCamera Matrimoniale Business\n",
+			"describe" : "1 letto matrimoniale"
+		}]};
+
+
+const input5 = [
+	{"open": "https://www.booking.com/hotel/it/holiday-inn-cosenza.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQ_YAQHoAQH4AQuIAgGoAgM;sid=9e5a2f72fb374b24b8be4033628b9dd1;dest_id=-116404;dest_type=city;dist=0;hapos=1;hpos=1;room1=A%2CA;sb_price_type=total;sr_order=popularity;srepoch=1550836160;srpvid=c4ce532049eb0091;type=total;ucfs=1&#hotelTmpl"},
+	{"scrape":
+	{"allrooms":["//*[@class='jqrt togglelink']"]}}];
+var output5 = { "allrooms":
+	[ "'\n\nCamera Economy Matrimoniale/Doppia con Letti Singoli\n'",
+		"'\n\nCamera Familiare\n'",
+		"'\n\nJunior Suite\n'",
+		"'\n\nSuite\n'",
+		"'\n\nCamera Matrimoniale Business\n'" ]};
+
+const input6 = [
+	{"open": "https://www.booking.com/searchresults.it.html?label=gen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQ_YAQHoAQH4AQuIAgGoAgM&sid=2f11f0f3ad146f3a648faa7e038f74a0&sb=1&src=searchresults&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Fsearchresults.it.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaHGIAQGYARS4AQfIAQ_YAQHoAQH4AQuIAgGoAgM%3Bsid%3D2f11f0f3ad146f3a648faa7e038f74a0%3Btmpl%3Dsearchresults%3Bclass_interval%3D1%3Bdest_id%3D-116404%3Bdest_type%3Dcity%3Bdtdisc%3D0%3Bgroup_adults%3D2%3Bgroup_children%3D0%3Binac%3D0%3Bindex_postcard%3D0%3Blabel_click%3Dundef%3Bno_rooms%3D1%3Boffset%3D0%3Bpostcard%3D0%3Broom1%3DA%252CA%3Bsb_price_type%3Dtotal%3Bshw_aparth%3D1%3Bslp_r_match%3D0%3Bsrc%3Dindex%3Bsrc_elem%3Dsb%3Bsrpvid%3D4910a725d83b0347%3Bss%3DCosenza%3Bss_all%3D0%3Bssb%3Dempty%3Bsshis%3D0%26%3B&ss=Crotone%2C+Calabria%2C+Italia&is_ski_area=&ssne=Cosenza&ssne_untouched=Cosenza&city=-116404&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&from_sf=1&search_pageview_id=4910a725d83b0347&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0&ac_position=0&ac_langcode=it&ac_click_type=b&dest_id=-116612&dest_type=city&place_id_lat=39.079899&place_id_lon=17.128599&search_pageview_id=4910a725d83b0347&search_selected=true&ss_raw=Crotone"},
+	{"scrape":
+	{"allhotel": {
+		"_forEach_" : ["//*[@data-hotelid]"],
+		"_extract_": {"name" : "./div[@class='sr_item_content sr_item_content_slider_wrapper ']/div[@class='sr_property_block_main_row']/div[@class='sr_item_main_block']/h3/a/span",
+			"review" : "./div[@class='sr_item_content sr_item_content_slider_wrapper ']/div[@class='sr_property_block_main_row']/div[@class='sr_item_review_block']/div/div[@class='\nsr-review-score\n']/a[@class='sr-review-score__link']/div/div[@class='bui-review-score__badge']"
+		}}}}];
+var output6 = { allhotel:
+	[ { name: '\nHelios Hotel\n', review: ' 8,1 ' },
+		{ name: '\nCorsonove\n', review: ' 9,0 ' },
+		{ name: '\nCentral Apartments\n', review: ' 9,2 ' },
+		{ name: ' \nAppartamento\n ', review: undefined },
+		{ name: '\nB&B Pitagora\n', review: ' 8,8 ' },
+		{ name: ' \nAppartamento\n ', review: ' 9,5 ' },
+		{ name: ' \nVilla\n ', review: ' 9,6 ' },
+		{ name: '\nAlma b&b\n', review: ' 8,8 ' },
+		{ name: '\nB&B L\'ORIZZONTE\n', review: ' 8,9 ' },
+		{ name: '\nB&B Elisa\n', review: ' 9,2 ' },
+		{ name: '\nB&B Milu\'\n', review: ' 9,6 ' },
+		{ name: '\nJulia B&B\n', review: ' 9,2 ' },
+		{ name: '\nBed & Ship Archimediterraneo\n', review: ' 7,6 ' },
+		{ name: '\nSanta Veneranda\n', review: ' 9,5 ' },
+		{ name: '\nIl Diamante\n', review: ' 9,7 ' } ] };
+
 
 function main() {
-//	pushTestCase(input1);
-//	pushResult(output1);
-//	pushTestCase(input2);
-//	pushResult(output2);
-//	pushTestCase(input3);
-//	pushResult(output3);
+	pushTestCase(input1);
+	pushResult(output1);
+	pushTestCase(input2);
+	pushResult(output2);
+	pushTestCase(input3);
+	pushResult(output3);
 	pushTestCase(input4);
 	pushResult(output4);
+	pushTestCase(input5);
+	pushResult(output5);
+	pushTestCase(input6);
+	pushResult(output6);
 
 	var test =  [];
 	var result = [];
